978a5fb615bc8640c7e03b84a0b09fbbe2618ed3
Adarsh7448 <adarsh.madre2@gmail.com>
2024-03-03 22:02:10 +0530

    Create and Read for transaction created

--- application/controllers.py
+++ application/controllers.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, request 
 from flask import current_app as app
 from .models import *
+import datetime
 
 # http://127.0.0.1:5000/  base url
 # http://127.0.0.1:5000/userlogin 
@@ -17,7 +18,10 @@ def user_login():
         this_user = User.query.filter_by(username = u_name).first() # or .all()
         if this_user:
             if this_user.password == pwd:
-                return render_template("user_dash.html", u_name = u_name) 
+                if this_user.type == "admin":
+                    return redirect('/admin')
+                else:
+                    return redirect(f'/user/{this_user.id}') 
             else:
                 return "incorrect Password"
         else:
@@ -39,4 +43,34 @@ def user_register():
             return redirect('/userlogin') 
     return render_template('register.html')
 
+@app.route('/admin', methods=['GET', 'POST'])
+def admin_login():
+    admin = User.query.filter_by(type = "admin").first()
+    requested_trans = Transaction.query.filter_by(internal_status = "requested").all()
+    quoted_trans = Transaction.query.filter_by(internal_status = "quoted").all()
+    paid_trans = Transaction.query.filter_by(internal_status = "paid").all()
+    return render_template("admin_dash.html", requested_trans = requested_trans,
+                                              quoted_trans = quoted_trans,
+                                              paid_trans = paid_trans)
 
+@app.route('/user/<int:user_id>', methods=['GET', 'POST'])
+def user(user_id):
+    user = User.query.get(user_id)
+    transactions = user.trans
+    return render_template('user_dash.html', user = user, transactions = transactions)
+
+@app.route('/transaction_create/<int:user_id>', methods=['GET', 'POST'])
+def trans_create(user_id):
+    user = User.query.get(user_id)
+    if request.method == 'POST':
+        t_name = request.form.get('t_name')
+        t_type = request.form.get('t_type')
+        s_city = request.form.get('s_city')
+        d_city = request.form.get('d_city')
+        description = request.form.get('description')
+        new_trans = Transaction(t_name = t_name, t_type = t_type, t_date = datetime.datetime.now(), s_city = s_city, d_city = d_city, decription = description, user_id = user.id)
+        db.session.add(new_trans)
+        db.session.commit()
+        return redirect(f'/user/{user.id}')
+
+    return render_template('create_transac.html', user = user)
